430c32b9a79a4941e9b2f0c70859c8f56fe7aafa
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-10-07 12:18:40 +0300

    добавил приложение пользователей

--- config/settings.py
+++ config/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "rest_framework",
     "corsheaders",
-    "",
-    "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_spectacular",
+    "djoser",
 
     "habits",
     "telegramer",
