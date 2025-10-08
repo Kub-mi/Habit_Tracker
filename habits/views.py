b1297416e108051e57d06962da829162b4fa979a
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-10-08 10:09:56 +0300

    сериалайзер и вью

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Habit
+from .serializers import HabitSerializer
 
-# Create your views here.
+class HabitViewSet(viewsets.ModelViewSet):
+    queryset = Habit.objects.all().order_by("-created_at")
+    serializer_class = HabitSerializer
+    permission_classes = [permissions.IsAuthenticated]
